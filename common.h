f97028e6b7c5f474b231fc3bfb03656222267fb4
mhousser <mhousser@5b9ac4b1-7c3f-0410-9070-89a53124ee38>
2007-11-22 10:59:05 +0000

    More work on engine framework. Have basic moving block functioning.

--- common.h
+++ common.h
@@ -10,3 +10,7 @@ const char * GAME_TITLE = "Sword of Kings";
 // Default width and height of game window
 const int DEFAULT_WINDOW_WIDTH = 800;
 const int DEFAULT_WINDOW_HEIGHT = 600;
+
+// Width and height of the playing area in grid units
+const int GRID_WIDTH = 40;
+const int GRID_HEIGHT = 30;
